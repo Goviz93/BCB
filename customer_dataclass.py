e351e2c1200065bee567de5f94d3b57e52f21cbd
Goviz <vizro_93@outlook.com>
2023-11-04 21:08:39 -0600

    Feature - Customer dataclass migrated to an other python file.

---
+++ customer_dataclass.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Customer:
+    LastName: str
+    SecondLastName: str
+    Name: str
+    Birthday: str
+    Gender: str
+    phone: str
+    NroDoc: str
+    Address: str
+    email: str
+    USD: str
+    Job: str
+    Source: str
+    Destiny: str
+
+
