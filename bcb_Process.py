042b82a8494fa8305912d69679fc0307ebaeffe1
Goviz <vizro_93@outlook.com>
2023-11-15 21:27:22 -0600

    Feature - Licence and date validation.

--- bcb_Process.py
+++ bcb_Process.py
@@ -4,6 +4,9 @@ from scripts.spreadSheets import customers_data
 from scripts.getTime_API import validate_date_with_worldtimeapi
 
 
+
+
+
 def validateLicense(date):
     return validate_date_with_worldtimeapi(date)
 
@@ -23,6 +26,17 @@ def my_function():
 
 
 if __name__ == '__main__':
+    codes = { '4u2S84NSAaaNMP': '2023-11-14',
+              '5GsieqHkXJPyQD': '2023-12-02',
+              'cUf4UJio2Gqmo5': '2023-12-09',
+              'PNmKhS7BVyn2FZ': '2023-12-16',
+              '7M9RYy3f5X2NQW': '2023-12-23',
+              '4ZMo2MQx4BiA9T': '2023-12-30'
+              }
+    Token = input('Ingrese Token: ')
 
-    if validate_date_with_worldtimeapi('2023-11-14'):
-        my_function()
+    if Token in codes.keys():
+        if validate_date_with_worldtimeapi(codes.get(Token)):
+            my_function()
+    else:
+        print('Token Invalido')
